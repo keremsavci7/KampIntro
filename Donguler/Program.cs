2e107bf1dc15ecbb307b8a89dd469cffab3cb2b8
Kerem <Kerem@DESKTOP-6MSCSJG>
2022-03-28 17:05:55 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿//class Program
+//{
+    //static void Main(string[] args)
+    //{
+        //string kurs1 = "Yazılım Bla Bbal";
+        //string kurs2 = " Programlamaya Başlangıç";
+        //string kurs3 = "Java";
+
+        //array - dizi
+
+        string[] kurslar = new string[] { "Yazılım Bla Bbal" , "Programlamaya Başlangıç" , "Java" };
+
+
+
+        for (int i = 0; i<kurslar.Length; i++)
+        {
+
+            Console.WriteLine(kurslar[i]);
+
+        }
+       
+        foreach (string kurs in kurslar)
+        {
+
+
+            Console.WriteLine(kurs);
+
+        }
+
+
+
+
+    
+       
+
---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿string kategoriEtiketi = "Kategoriler";
+
+Console.WriteLine(kategoriEtiketi);
+
+int ogrenciSayisi = 32000;
+double faizOrani = 1.45;
+
+bool sistemeGirisYapmisMi = true;
+double dolarDun = 7.35;
+double dolarBugun = 7.45;
+
+if(dolarDun>dolarBugun)
+{
+    Console.WriteLine("Azalış Butonu");
+
+
+}
+else if (dolarDun<dolarBugun)
+{
+    Console.WriteLine("Artış Oku");
+
+
+}
+else
+{
+    Console.WriteLine("Değişmedi Butonu");
+}
+
+
+
+
+if (sistemeGirisYapmisMi == true)
+{
+
+  Console.WriteLine("Kullanıcı Ayarları Butonu");
+
+}
+else
+{
+    Console.WriteLine("Giriş Yap Butonu");
+}
